41bae3c9f604e17c8b6caa2edc2d032910596021
Himani Sharma <himanisharma.hs05@gmail.com>
2026-03-28 09:41:43 +0000

    Sync LeetCode submission Runtime - 81 ms (34.45%), Memory - 146.2 MB (98.69%)

---
+++ solutions/0523-continuous-subarray-sum/solution.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool checkSubarraySum(vector<int>& nums, int k) {
+        unordered_map<int, int> mp;
+        mp[0] = -1;
+
+        int sum = 0;
+
+        for(int i = 0; i < nums.size(); i++){
+            sum += nums[i];
+            int mod = sum % k;
+
+            if(mp.find(mod) != mp.end()){
+                if(i - mp[mod] >= 2){
+                    return true;
+                }
+            } else {
+                mp[mod] = i;
+            }
+        }
+        return false;
+    }
+};
