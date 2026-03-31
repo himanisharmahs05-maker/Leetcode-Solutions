291bae5744e6b9f620dccde19f16ce0dcb625c08
Himani Sharma <himanisharma.hs05@gmail.com>
2026-03-31 07:36:14 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 24 MB (33.41%)

---
+++ solutions/0056-merge-intervals/solution.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        vector<vector<int>> ans;
+
+        sort(intervals.begin(), intervals.end());
+
+        int start1 = intervals[0][0];
+        int end1   = intervals[0][1];
+
+        for(int i = 1; i < intervals.size(); i++){
+            int start2 = intervals[i][0];
+            int end2   = intervals[i][1];
+
+            if(end1 >= start2){
+                end1 = max(end1, end2);
+            }
+            else{
+                ans.push_back({start1, end1});
+                start1 = start2;
+                end1 = end2;
+            }
+        }
+
+        // last interval add karna zaroori hai
+        ans.push_back({start1, end1});
+
+        return ans;
+    }
+};
