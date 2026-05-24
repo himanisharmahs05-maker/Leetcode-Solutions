8d45089963815d60b1da8f433cac10db344d11cd
Himani Sharma <himanisharma.hs05@gmail.com>
2026-05-24 16:27:31 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 7.7 MB (91.83%)

---
+++ solutions/0168-excel-sheet-column-title/solution.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    string convertToTitle(int columnNumber) {
+        string result = "";
+        
+        while (columnNumber > 0) {
+            columnNumber--;  // Adjust for 1-based indexing
+            
+            char ch = 'A' + (columnNumber % 26);
+            result += ch;
+            
+            columnNumber /= 26;
+        }
+        
+        reverse(result.begin(), result.end());
+        return result;
+    }
+};
