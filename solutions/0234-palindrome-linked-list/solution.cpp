74584934213a64a24d1596e3c016136cbdc5859f
Himani Sharma <himanisharma.hs05@gmail.com>
2026-06-12 15:22:22 +0000

    Sync LeetCode submission Runtime - 4 ms (48.84%), Memory - 118 MB (64.15%)

---
+++ solutions/0234-palindrome-linked-list/solution.cpp
@@ -0,0 +1,55 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode* prev = NULL;
+        ListNode* curr = head;
+
+        while (curr) {
+            ListNode* nextNode = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = nextNode;
+        }
+
+        return prev;
+    }
+
+    bool isPalindrome(ListNode* head) {
+        if (!head || !head->next)
+            return true;
+
+        // Find middle
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        while (fast && fast->next) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+
+        // Reverse second half
+        ListNode* secondHalf = reverseList(slow);
+        ListNode* firstHalf = head;
+
+        // Compare both halves
+        while (secondHalf) {
+            if (firstHalf->val != secondHalf->val)
+                return false;
+
+            firstHalf = firstHalf->next;
+            secondHalf = secondHalf->next;
+        }
+
+        return true;
+    }
+};
