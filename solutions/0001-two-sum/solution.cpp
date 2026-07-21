041f5876896af810b8c7df2ac87ce6bea6fe9631
Himani Sharma <himanisharma.hs05@gmail.com>
2026-07-21 16:50:36 +0000

    Sync LeetCode submission Runtime - 3 ms (67.50%), Memory - 14.8 MB (58.63%)

--- solutions/0001-two-sum/solution.cpp
+++ solutions/0001-two-sum/solution.cpp
@@ -1,20 +1,19 @@
 class Solution {
 public:
-    vector<int> twoSum(vector<int>& nums, int target) 
-    {
+    vector<int> twoSum(vector<int>& nums, int target) {
         unordered_map<int,int>mp;
 
-        for(int i=0;i<nums.size();i++)
-        {
-            int complement= target-nums[i];
+        for(int i=0;i<nums.size();i++){
+            int need=target-nums[i];
 
-            if(mp.find(complement)!=mp.end())
-            {
-                return {mp[complement],i};
+            if(mp.find(need)!=mp.end()){
+                return {mp[need],i};
 
             }
             mp[nums[i]]=i;
+
         }
         return {};
+        
     }
 };
