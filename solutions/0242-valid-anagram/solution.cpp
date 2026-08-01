f1ecebdd1944a2fc5e6d9be06fecbfee3c9d96fc
Himani Sharma <himanisharma.hs05@gmail.com>
2026-08-01 06:20:47 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 9.7 MB (75.02%)

--- solutions/0242-valid-anagram/solution.cpp
+++ solutions/0242-valid-anagram/solution.cpp
@@ -1,22 +1,25 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        if(s.size()!= t.size()){
+        if(s.length()!=t.length()){
             return false;
         }
-        vector<int>count(26,0);
+        int count[26]={0};
 
-        for(char ch:s){
-            count[ch-'a']++;
+        for(int i=0;i<s.length();i++){
+            count[s[i] - 'a']++;
+            count[t[i]-'a']--;   
+
         }
-        for(char ch:t){
-            count[ch-'a']--;
-        }
-        for(int freq:count){
-            if(freq!=0){
+        for(int i=0;i<26;i++){
+            if(count[i]!=0){
                 return false;
             }
+
         }
         return true;
+            
+
+        
     }
 };
