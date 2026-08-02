7b16819dfb88980cce28d53faa34b3b9bcb3bd5b
Himani Sharma <himanisharma.hs05@gmail.com>
2026-08-02 06:17:46 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 9.8 MB (82.47%)

---
+++ solutions/0078-subsets/solution.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    void solve(vector<int>&nums,int index, vector<int>&current, vector<vector<int>>&ans){
+        if(index==nums.size()){
+            ans.push_back(current);
+            return;
+        }
+        //include
+        current.push_back(nums[index]);
+        solve(nums,index+1,current,ans);
+
+        //backtrack
+        current.pop_back();
+
+        //dont include
+        solve(nums,index+1,current,ans);
+    }
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>>ans;
+        vector<int>current;
+
+        solve(nums,0,current,ans);
+        return ans;
+        
+    }
+};
