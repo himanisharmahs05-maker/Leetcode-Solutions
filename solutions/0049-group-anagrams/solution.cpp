25610241414eec798585785c8941bcfdbab142ee
Himani Sharma <himanisharma.hs05@gmail.com>
2026-08-25 19:31:46 +0000

    Sync LeetCode submission Runtime - 36 ms (17.68%), Memory - 36.3 MB (5.63%)

--- solutions/0049-group-anagrams/solution.cpp
+++ solutions/0049-group-anagrams/solution.cpp
@@ -1,20 +1,21 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        unordered_map<string, vector<string>> mp;
+        map<vector<int>,vector<string>>mp;
+        for(string s: strs){
+            vector<int>freq(26,0);
+            for(char ch:s){
+                freq[ch-'a']++;
+            }
+            mp[freq].push_back(s);
 
-        for (string s : strs) {
-            string key = s;
-            sort(key.begin(), key.end());
-            mp[key].push_back(s);
         }
-
-        vector<vector<string>> ans;
-
-        for (auto &it : mp) {
+        vector<vector<string>>ans;
+        for(auto it: mp){
             ans.push_back(it.second);
         }
-
         return ans;
+        
+        
     }
 };
