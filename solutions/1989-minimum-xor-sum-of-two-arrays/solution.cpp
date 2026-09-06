a5dbf34094b7f24e69e22ff6981d181e4ecbcb91
Himani Sharma <himanisharma.hs05@gmail.com>
2026-09-06 12:09:06 +0000

    Sync LeetCode submission Runtime - 7 ms (98.14%), Memory - 11.9 MB (71.89%)

---
+++ solutions/1989-minimum-xor-sum-of-two-arrays/solution.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int minimumXORSum(vector<int>& nums1, vector<int>& nums2) {
+        int n = nums1.size();
+        int N = 1 << n;
+        
+        vector<int> dp(N, INT_MAX);
+        dp[0] = 0;
+
+        for (int mask = 0; mask < N; mask++) {
+            int i = __builtin_popcount(mask);
+
+            for (int j = 0; j < n; j++) {
+                if (!(mask & (1 << j))) {
+                    int newMask = mask | (1 << j);
+                    dp[newMask] = min(dp[newMask],
+                        dp[mask] + (nums1[i] ^ nums2[j]));
+                }
+            }
+        }
+
+        return dp[N - 1];
+    }
+};
